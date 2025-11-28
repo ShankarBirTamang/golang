edd924fb097b2b190958511f533c5fa7c5a9b982
ShankarBirTamang <sankar.jt19@gmail.com>
2025-11-28 21:28:21 +0545

    postgres docker go

---
+++ 4_go-postgres-docker-app/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// connStr := "host=localhost user=postgres password=sankar dbname=testdb sslmode=disable"
+	connStr := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Cannot connect to DB:", err)
+	}
+
+	fmt.Println("Connected to Postgres successfully!")
+}
